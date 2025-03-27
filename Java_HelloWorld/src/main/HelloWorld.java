858942d94354e39d4bd4c6a3d31738e6cc6b701e
Csatti01 <cseh.attila1990@gmail.com>
2025-03-27 18:42:03 +0100

    Init Project

---
+++ Java_HelloWorld/src/main/HelloWorld.java
@@ -0,0 +1,29 @@
+package main;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		
+		System.out.println("5+3 = " + (5+3));
+		
+		
+		System.out.println(5+3);
+		System.out.println(3-5);
+		System.out.println("5*3 = " + (5*3));
+		System.out.println((50/10)/5);
+		
+		
+		System.err.println("Hello World!");
+		
+		System.out.println(5>3);
+		System.out.println(3>3);
+		System.out.println(5>=3);
+		System.out.println(1<2);
+		System.out.println(8<=4);
+		System.out.println(0!=15);
+		System.out.println(9==9);
+		System.out.println(5==7);
+	}
+
+}
